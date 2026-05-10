3c6ce3e4879c7868e49649800173bfc2af1bc3ff
Karim <2301101@eng.asu.edu.eg>
2026-05-10 14:40:08 +0300

    nothing

--- obj/Debug/net10.0-windows/Compiler Project.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/Compiler Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Compiler Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbdebe9731f0b43cae8b58cd3a73be5e0d96e96b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+901b5726c506022dc62fe668e2e1ee80f4965132")]
 [assembly: System.Reflection.AssemblyProductAttribute("Compiler Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Compiler Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- obj/Release/net10.0-windows/Compiler Project.AssemblyInfo.cs
+++ obj/Release/net10.0-windows/Compiler Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Compiler Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbdebe9731f0b43cae8b58cd3a73be5e0d96e96b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb671e865e28754dd04198ff67ae8755b05dcbe8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Compiler Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Compiler Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
